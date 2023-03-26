71b879a0b3c56377d65fd1bc24aae37d948808f1
FoxyYTs <foxy200442@gmail.com>
2023-03-26 18:51:35 -0500

    asdf

---
+++ Repositorio/TrabajosDeClase/listas/Pedro.java
@@ -0,0 +1,18 @@
+package Repositorio.TrabajosDeClase.listas;
+
+public class Pedro {
+    public static void main(String[] args) {
+        Nodo nodo1 = new Nodo(0);
+        Nodo head = nodo1;
+        Nodo pointer = head;
+        
+        for (int i = 0; i <= 10; i++) {
+            Nodo nodi = new Nodo(i);
+        }
+
+        for (int i = 0; pointer != null; i++) {
+            pointer = pointer.siguiente;
+            System.out.println(pointer);
+        }
+    }
+}
--- Repositorio/TrabajosDeClase/listas/eje1.java
+++ Repositorio/TrabajosDeClase/listas/eje1.java
@@ -1,15 +1,11 @@
 package Repositorio.TrabajosDeClase.listas;
 
+import java.util.*;
 public class eje1 {
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
         ListasLigadas lista = new ListasLigadas();
 
-        lista.insertar(5);
-        lista.insertar(10);
-        lista.insertar(15);
-        lista.insertar(20);
-        lista.insertar(25);
-
-        lista.mostrar();
+        
     }
 }
