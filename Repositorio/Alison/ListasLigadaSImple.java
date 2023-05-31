402113adeb72a1ef49bd752cc63adce013cdbff2
FoxyYTs <foxy200442@gmail.com>
2023-05-31 13:37:00 -0500

    asda

--- Repositorio/Alison/ListasLigadaSImple.java
+++ Repositorio/Alison/ListasLigadaSImple.java
@@ -1,20 +1,41 @@
 package Alison;
 
 public class ListasLigadaSImple {
-    Nodo head;
-    public void insertar(int dato){
-        Nodo nuevo = new Nodo(dato);
-        if(head == null){
+    Nodo head = null;
+    Nodo head2=null;
+
+    public void eliminar(Nodo direccion){
+         Nodo pointer=head;
+         while(pointer != null){
+            if(pointer.next==direccion){
+                pointer.next = pointer.next.next;
+            }
+            pointer=pointer.next;
+         }
+
+    }
+
+    public void listas(){
+        Nodo pointer=head;
+        Nodo poiterNodo=head2;
+        while(pointer.next != null){
+            pointer = pointer.next;
+        }
+        pointer.next = head2;
+    }
+        
+    public void insertar(int valor){
+        Nodo nuevo = new Nodo(valor);
+        if (head == null){
             head = nuevo;
         }else{
-
-
-    
-            
+            Nodo pointer = head;
+            while (pointer.next != null){
+                pointer = pointer.next;
+            }
+            pointer.next = nuevo;
         }
     }
 
 
-
-    
 }
--- Repositorio/Alison/Nodo.java
+++ Repositorio/Alison/Nodo.java
@@ -1,11 +1,17 @@
 package Alison;
 
 public class Nodo {
-    public int dato;
-    public Nodo siguiente;
-    public Nodo(int dato){
-        this.dato=dato;
-        this.siguiente=null; 
+    int valor;
+    Nodo next;
+    public Nodo(int valor){
+        this.valor=valor;
+        this.next=null;
+
+        
     }
+    
+        
 }
+
+
 
