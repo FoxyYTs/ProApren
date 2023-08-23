48d1923a62ceed87554cf085c177e4b9312343e2
FoxyYTs <foxy200442@gmail.com>
2023-08-23 17:58:58 -0500

    asdasd

---
+++ Repositorio/AlgoritmosYProgramacion/TercerSemestre/Taller2/Funciones.java
@@ -0,0 +1,38 @@
+package AlgoritmosYProgramacion.TercerSemestre.Taller2;
+
+public class Funciones {
+    double pBase = 85000,fuel = pBase*0.15, admin = pBase*0.07, seguro = pBase*0.10, total = pBase + fuel + admin + seguro, vipP = (total*0.65) + total;
+
+    int sillaVip = 5, silla = 8, cantSilla = sillaVip + silla;
+
+    int contadorVip = 0, contadorOcupado = 0;
+
+    Nodo[] puestos = new Nodo[12];
+
+    public String valor(){
+        return "El Valor Base del tiquete es \nBASE: " + (int) pBase + "\nCOMBUSTIBLE 15%: " + (int) fuel + "\nTASA ADMINISTRACION 7%: " + (int) admin + "\nSEGURO 10%: " + (int) seguro + "\nTOTAL: " + (int) total + "\nVIP 65% + TOTAL: " + (int) vipP ;
+    }
+
+    public void disponibilidad(){
+        if (contadorOcupado <= 0) {
+            System.out.println("El Avion esta Vacio");
+            return;
+        }
+        for (int i = 0; i <= cantSilla; i++) {
+            System.out.println("Puesto #" + i + " Nombre Pasajero: " + puestos[i].dueño + " VIP: " + puestos[i].vip);
+        }
+    }
+
+    public void compra(String nombre, Boolean vip){
+        if (vip) {
+            System.out.println("Se a Comprado un Tiqued a nombre: " + nombre + "\nVIP: " + vip + "\nTOTAL: " + vip);
+            contadorVip++;
+            puestos[contadorOcupado] = new Nodo(nombre, vip, vipP);
+            contadorOcupado++;
+        } else {
+            System.out.println("Se a Comprado un Tiqued a nombre: " + nombre + "\nVIP: " + vip + "\nTOTAL: " + total);
+            puestos[contadorOcupado] = new Nodo(nombre, vip, total);
+            contadorOcupado++;
+        }
+    }
+}
--- Repositorio/AlgoritmosYProgramacion/TercerSemestre/Taller2/Menu.java
+++ Repositorio/AlgoritmosYProgramacion/TercerSemestre/Taller2/Menu.java
@@ -1,5 +1,84 @@
 package AlgoritmosYProgramacion.TercerSemestre.Taller2;
 
+import java.util.Scanner;
+
 public class Menu {
-    
+    public static void main(String[] args) {
+
+        Funciones fun = new Funciones();
+        Scanner leer = new Scanner(System.in);
+
+        String opcion = "",nombre,vip="N";
+        Boolean seguir = true, vipB = false;
+
+        System.out.println("Bienvenido al programa de Aerolinea");
+        leer.nextLine();
+        while (seguir){
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.print("Que operacion quiere realizar\n1) Comprar Tiquete\n2) Mostrar Ocupacion del Avion\n3) Mostrar Valor a Pagar por Asiento\n4) Salir\nEleccion: ");
+            opcion = leer.nextLine();
+            switch (opcion) {
+                case "1":
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println("Cargando Registro...");
+                    leer.nextLine();
+                    System.out.print("Ingrese el nombre de la persona que tendra este asiento: ");
+                    nombre = leer.nextLine();
+                    if (fun.contadorVip < 5) {
+                        System.out.print("Quiere Comprar un Tiquete VIP (Y/N): ");
+                        vip = leer.nextLine();
+                        if (vip != "Y" || vip != "N") {
+                            System.out.println("OPCION NO VALIDA");
+                            leer.nextLine();
+                            break;
+                        } else if(vip == "Y"){
+                            vipB = true;
+                        } else {
+                            vipB = false;
+                        }
+                    } else {
+                        System.out.println("Los Asientos VIP estan llenos");
+                    }
+                    fun.compra(nombre, vipB);
+                    break;
+                
+                case "2":
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println("Verificando Puestos...");
+                    leer.nextLine();
+                    fun.disponibilidad();
+                    leer.nextLine();
+                    break;
+
+                case "3":
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println("Calculando Precios...");
+                    leer.nextLine();
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println(fun.valor());
+                    leer.nextLine();
+                    break;
+
+                case "4":
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println("Saliendo...");
+                    seguir = false;
+                    leer.nextLine();
+                    break;
+            
+                default:
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+                    System.out.println("Opcion no valida");
+                    leer.nextLine();
+                    break;
+            }
+        }
+    }
 }
