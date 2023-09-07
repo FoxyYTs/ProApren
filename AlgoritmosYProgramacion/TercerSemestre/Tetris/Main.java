d8d5a29aaf56b9b40e9f4f6a95eef207ab0ef2ad
FoxyYTs <foxy200442@gmail.com>
2023-09-06 22:32:47 -0500

    gq

--- AlgoritmosYProgramacion/TercerSemestre/Tetris/Main.java
+++ AlgoritmosYProgramacion/TercerSemestre/Tetris/Main.java
@@ -1,11 +1,33 @@
 package AlgoritmosYProgramacion.TercerSemestre.Tetris;
+
+
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+
 public class Main {
     public static void main(String[] args) {
-        oidoEvento kle = new oidoEvento();
+        KeyAdapter keyAdapter = new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                char keyChar = e.getKeyChar();
+                int keyCode = e.getKeyCode();
 
-        kle
+                System.out.println("Tecla presionada: " + keyChar);
+                System.out.println("Código de tecla: " + keyCode);
+            }
+        };
+
+        System.out.println("Presiona una tecla (Presiona Ctrl+C para salir)");
+
+        // Agregar el KeyAdapter al sistema
+        System.in.addKeyListener(keyAdapter);
+
+        // Mantener el programa en ejecución
+        try {
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
 }
+
