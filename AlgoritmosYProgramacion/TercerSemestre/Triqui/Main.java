fe263d0670eac5f0f4bd1bcc13b23a2a8eae7851
FoxyYTs <foxy200442@gmail.com>
2023-09-26 17:20:07 -0500

    inafa

---
+++ AlgoritmosYProgramacion/TercerSemestre/Triqui/Main.java
@@ -0,0 +1,8 @@
+package AlgoritmosYProgramacion.TercerSemestre.Triqui;
+
+public class Main {
+    public static void main(String[] args) {
+        Tablero tab = new Tablero();
+        tab.juego();
+    }
+}
---
+++ AlgoritmosYProgramacion/TercerSemestre/Triqui/Tablero.java
@@ -0,0 +1,93 @@
+package AlgoritmosYProgramacion.TercerSemestre.Triqui;
+
+import java.util.Scanner;
+
+public class Tablero {
+    private static Scanner leer = new Scanner(System.in);
+    private int x = 4;
+    private String triqui[][] = new String[x][x], toca;
+    private int posX, posY, turno = 0;
+    private boolean gano = false;
+    public void juego(){
+        inicio();
+        
+        while (turno < 9 && !gano) {
+            System.out.flush();
+            System.out.print("\033[H\033[2J");
+            print();
+            gano();
+            if (gano) {
+                return;
+            }
+            System.out.print("COLUMNA: ");
+            posX = Integer.parseInt(leer.nextLine());
+            System.out.print("FILA:");
+            posY = Integer.parseInt(leer.nextLine());
+            jugada();
+        }
+    }
+    private void jugada(){
+        if (triqui[posY][posX].equals("| |")) {
+            turno++;
+            triqui[posY][posX] = "|" + toca + "|";
+        }
+    }
+    private void gano(){
+        for (int i = 1; i < x; i++) {
+            if (triqui[i][2].equals(triqui[i][1]) && triqui[i][2].equals(triqui[i][3]) && !triqui[i][2].equals("| |")) {
+                System.out.println("Gano la: " + triqui[i][2]);
+                gano = true;
+                return;
+            }
+        }
+        for (int i = 1; i < x; i++) {
+            if (triqui[2][i].equals(triqui[1][i]) && triqui[2][i].equals(triqui[3][i]) && !triqui[2][i].equals("| |")) {
+                System.out.println("Gano la: " + triqui[1][i]);
+                gano = true;
+                return;
+            }
+        }
+        if (triqui[2][2].equals(triqui[1][1]) && triqui[2][2].equals(triqui[3][3]) && !triqui[2][2].equals("| |")) {
+            System.out.println("Gano la: " + triqui[2][2]);
+            gano = true;
+            return;
+        }
+        if (triqui[2][2].equals(triqui[3][1]) && triqui[2][2].equals(triqui[1][3]) && !triqui[2][2].equals("| |")) {
+            System.out.println("Gano la: " + triqui[2][2]);
+            gano = true;
+            return;
+        }
+    }
+
+    private void inicio(){
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if (j == 0 && i == 0) {
+                    triqui[i][j] = "X  ";
+                } else if (j == 0) {
+                    triqui[i][j] = i + " ";
+                } else if (i == 0){
+                    triqui[i][j] = j + "  ";
+                } else {
+                    triqui[i][j] = "| |";
+                }
+            }
+        }
+    }
+
+    public void print(){
+        if (turno % 2 == 0){
+            toca = "X";
+            triqui[0][0] = "X  ";
+        } else if (turno % 2 != 0){
+            toca = "O";
+            triqui[0][0] = "O  ";
+        }
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                System.out.print(triqui[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
