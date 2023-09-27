62a1a1c7dbe2544efc45036780e01e017b6d915a
FoxyYTs <foxy200442@gmail.com>
2023-09-26 19:13:27 -0500

    dada

--- AlgoritmosYProgramacion/TercerSemestre/Triqui/Tablero.java
+++ AlgoritmosYProgramacion/TercerSemestre/Triqui/Tablero.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public class Tablero {
     private static Scanner leer = new Scanner(System.in);
-    private int x = 4;
-    private String triqui[][] = new String[x][x], toca;
+    private final int X = 4;
+    private String triqui[][] = new String[X][X], toca;
     private int posX, posY, turno = 0;
     private boolean gano = false;
     public void juego(){
         inicio();
         
         while (turno < 9 && !gano) {
-            System.out.flush();
-            System.out.print("\033[H\033[2J");
+            clearConsole();
             print();
             gano();
             if (gano) {
@@ -25,7 +24,13 @@ public class Tablero {
             posY = Integer.parseInt(leer.nextLine());
             jugada();
         }
+        
+        
     }
+    private void clearConsole() {
+            System.out.flush();
+            System.out.print("\033[H\033[2J");
+        }
     private void jugada(){
         if (triqui[posY][posX].equals("| |")) {
             turno++;
@@ -33,14 +38,14 @@ public class Tablero {
         }
     }
     private void gano(){
-        for (int i = 1; i < x; i++) {
+        for (int i = 1; i < X; i++) {
             if (triqui[i][2].equals(triqui[i][1]) && triqui[i][2].equals(triqui[i][3]) && !triqui[i][2].equals("| |")) {
                 System.out.println("Gano la: " + triqui[i][2]);
                 gano = true;
                 return;
             }
         }
-        for (int i = 1; i < x; i++) {
+        for (int i = 1; i < X; i++) {
             if (triqui[2][i].equals(triqui[1][i]) && triqui[2][i].equals(triqui[3][i]) && !triqui[2][i].equals("| |")) {
                 System.out.println("Gano la: " + triqui[1][i]);
                 gano = true;
@@ -60,8 +65,8 @@ public class Tablero {
     }
 
     private void inicio(){
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < x; j++) {
+        for (int i = 0; i < X; i++) {
+            for (int j = 0; j < X; j++) {
                 if (j == 0 && i == 0) {
                     triqui[i][j] = "X  ";
                 } else if (j == 0) {
@@ -83,8 +88,8 @@ public class Tablero {
             toca = "O";
             triqui[0][0] = "O  ";
         }
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < x; j++) {
+        for (int i = 0; i < X; i++) {
+            for (int j = 0; j < X; j++) {
                 System.out.print(triqui[i][j]);
             }
             System.out.println();
