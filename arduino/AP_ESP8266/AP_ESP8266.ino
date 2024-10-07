a585bf7ccb4cee6393dc1fbe7c3fac4016502d3d
FoxyYTs <foxy200442@gmail.com>
2024-10-07 13:20:37 -0500

    Lets Go

--- arduino/AP_ESP8266/AP_ESP8266.ino
+++ arduino/AP_ESP8266/AP_ESP8266.ino
@@ -1,126 +1,56 @@
-/*****************************************
+/****************************************
  * Include Libraries
  ****************************************/
-#include <ESP8266WiFi.h>
-#include <ConfigManager.h>
-#include <PubSubClient.h>
+#include "UbidotsESPMQTT.h"
 
 /****************************************
  * Define Constants
  ****************************************/
-namespace{
-  const char * AP_NAME = "Ubidots Access Point"; // Assigns your Access Point name
-  const char * MQTT_SERVER = "industrial.api.ubidots.com"; 
-  const char * TOKEN = "BBUS-bXgmuWeuji822RqGKmFwSHXva4h8hO"; // Assigns your Ubidots TOKEN
-  const char * DEVICE_LABEL = "my-device"; // Assigns your Device Label
-  const char * VARIABLE_LABEL = "my-variable"; // Assigns your Variable Label
-  int SENSOR = A0;
-  int LED = 16;
-  int PIN_RESET = 05;
-}
+#define TOKEN "BBUS-bXgmuWeuji822RqGKmFwSHXva4h8hO" // Your Ubidots TOKEN
+#define WIFINAME "DAZA" //Your SSID
+#define WIFIPASS "1022002153" // Your Wifi Pass
+#define DEVICE_LABEL  "Aparatus"  // Put here your Ubidots device label
+#define VARIABLE_LABEL  "temperatura"  // Put here your Ubidots variable label
 
-char topic[150];
-char payload[50];
-String clientMac = "";
-unsigned char mac[6];
-
-struct Config {
-  char name[20];
-  bool enabled;
-  int8 hour;
-} config;
-
-/****************************************
- * Initialize a global instance
- ****************************************/
-WiFiClient espClient;
-PubSubClient client(espClient);
-ConfigManager configManager;
-
+Ubidots client(TOKEN);
 
 /****************************************
  * Auxiliar Functions
  ****************************************/
 
-void callback(char* topic, byte* payload, unsigned int length){
-  
-}
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect(clientMac.c_str(), TOKEN, NULL)) {
-      Serial.println("connected");
-      break;
-      } else {
-        configManager.reset();
-        Serial.print("failed, rc=");
-        Serial.print(client.state());
-        Serial.println(" try again in 3 seconds");
-        for(uint8_t Blink=0; Blink<=3; Blink++){
-          digitalWrite(LED, LOW);
-          delay(500);
-          digitalWrite(LED, HIGH);
-          delay(500);
-        }
-      }
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
   }
-}
-
-String macToStr(const uint8_t* mac) {
-  String result;
-  for (int i = 0; i < 6; ++i) {
-    result += String(mac[i], 16);
-    if (i < 5)result += ':';
-  }
-  return result;
+  Serial.println();
 }
 
 /****************************************
  * Main Functions
  ****************************************/
+
 void setup() {
+  // put your setup code here, to run once:
+  //client.ubidotsSetBroker("industrial.api.ubidots.com"); // Sets the broker properly for the business account
+  client.setDebug(true); // Pass a true or false bool value to activate debug messages
   Serial.begin(115200);
-  /* Declare PINs as input/outpu */
-  pinMode(SENSOR, INPUT);
-  pinMode(PIN_RESET, INPUT);
-  pinMode(LED, OUTPUT);
-
-  /* Assign WiFi MAC address as MQTT client name */
-  WiFi.macAddress(mac);
-  clientMac += macToStr(mac);
-
-  /* Access Point configuration */
-  configManager.setAPName(AP_NAME);
-  configManager.addParameter("name", config.name, 20);
-  configManager.addParameter("enabled", &config.enabled);
-  configManager.addParameter("hour", &config.hour);
-  configManager.begin(config);
-
-  /* Set Sets the server details */
-  client.setServer(MQTT_SERVER, 1883);
-  client.setCallback(callback);
-  
-  /* Build the topic request */
-  sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
-}
-
-void loop() {  
-  configManager.reset();
-  configManager.loop();    
-  
-  /* MQTT client reconnection */
-  if (!client.connected()) {
-      reconnect();
+  client.wifiConnection(WIFINAME, WIFIPASS);
+  client.begin(callback);
   }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(!client.connected()){
+      client.reconnect();
+      }
   
-  /* Sensor Reading */
-  int value = analogRead(SENSOR);
-  /* Build the payload request */
-  sprintf(payload, "{\"%s\": %d}", VARIABLE_LABEL, value);
-  /* Publish sensor value to Ubidots */ 
-  client.publish(topic, payload);
+  float value1 = analogRead(0);
+  //float value2 = analogRead(2)
+  client.add("temperatura", value1);
+  //client.add("status", value2);
+  client.ubidotsPublish("aparatus");
   client.loop();
-  delay(5000);
 }
