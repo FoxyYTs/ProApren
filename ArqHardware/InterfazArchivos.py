4b15f549e7b8daac34803299e9199b5cf815caf7
FoxyYTs <foxy200442@gmail.com>
2024-10-28 12:50:19 -0500

    a

--- ArqHardware/InterfazArchivos.py
+++ ArqHardware/InterfazArchivos.py
@@ -4,9 +4,8 @@ import struct
 with open('archivo_binario.bin', 'rb') as f:
     
 
-    datos_leidos = f.read()
-    entero_leido = struct.unpack('>I', datos_leidos)[0]
-    cadena_binaria_recuperada = bin(entero_leido)[2:].zfill()  # Ajustar la longitud si es necesario
-    print(cadena_binaria_recuperada)
+    datos = f.read()
+    datos_leidos = pickle.loads(datos)
+    print(datos_leidos)
 
     
--- ArqHardware/a.py
+++ ArqHardware/a.py
@@ -2,16 +2,25 @@ import json
 import pickle
 import struct
 
-diccionario = {"e": "00", "l": "01", " ": "100", "a": "1010", "h": "1011", "s": "11"}
+huffman_code = {"e": "00", "l": "01", " ": "100", "a": "1010", "h": "1011", "s": "11"}
 cadena_bits = "01010110101100011111100111000110"
 
-diccionario_str = pickle.dumps(diccionario)
-entero = int(cadena_bits, 2)
-datos_empaquetados = struct.pack('>I', entero)
 
+entero = hex(int(cadena_bits, 2))
+
+diccionario = json.dumps(huffman_code)
+
+guardado = [str(diccionario), str(len(cadena_bits)), str(entero)]
+
+test = "\n".join(guardado)
+
+datos = pickle.dumps(guardado)
+
+print(datos)
 
 
 # Escribir los datos en un archivo binario
 with open('archivo_binario.bin', 'wb') as f:
-    f.write(datos_empaquetados)
+
+    f.write(datos)
     
