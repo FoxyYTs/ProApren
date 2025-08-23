9d2ecd425ddcf9ecde4f8084a455f3931d0e2c7e
FoxyYTs <64937979+FoxyYTs@users.noreply.github.com>
2025-08-22 21:27:22 -0500

    a

--- ProgramacionPropia/DJango/learndjango/core/urls.py
+++ ProgramacionPropia/DJango/learndjango/core/urls.py
@@ -5,9 +5,9 @@ from .views import my_view, my_test_view, author_view, CarListView
 
 
 urlpatterns = [
-    path('/listado', my_view),
-    path('/detalle/<int:id>', my_test_view),
-    path('/marcas/<str:brand>', my_test_view),
-    path('/autor/<int:id>', author_view),
-    path('/clist', CarListView.as_view()),
+    path('listado', my_view),
+    path('detalle/<int:id>', my_test_view),
+    path('marcas/<str:brand>', my_test_view),
+    path('autor/<int:id>', author_view),
+    path('clist', CarListView.as_view()),
 ]
