75d13bad71e710a29e4de0ea9f3a858accfd25d0
FoxyYTs <foxy200442@gmail.com>
2025-10-10 07:25:17 -0500

    `Removed Huffman compression and decompression script`

---
+++ ProgramacionPropia/python/Listas/simple.py
@@ -0,0 +1,10 @@
+class nodo:
+    def __init__(self, nombre, documento, tipo_atencion):
+        self.nombre = nombre
+        self.documento = documento
+        self.tipo_atencion
+        self.siguiente = None
+
+class listaPacientes:
+    def __init__(self):
+        
