13cf7dfa0696aeb0937bd6d5d5c83688fcc11605
FoxyYTs <foxy200442@gmail.com>
2026-02-08 09:52:32 -0500

    a

---
+++ Universidad/AnalisisNumerico/secuencia.py
@@ -0,0 +1,20 @@
+
+def secuencia():
+    x = int(input("Ingrese el valor inicial: "))
+    for i in range(1, int(input("Ingrese el numero de iteraciones: "))+1):
+        x = x ** 2 - 1 
+        print("Iteracion: ",i ," Valor de x: ", x)
+
+def secuencia2():
+    x = int(input("Ingrese el valor inicial: "))
+    for i in range(1, int(input("Ingrese el numero de iteraciones: "))+1):
+        x = 2*(x ** 2) - 1 
+        print("Iteracion: ",i ," Valor de x: ", x)
+        
+if __name__ == "__main__":
+    opcion = int(input("1) para la secuencia x^2 - 1 \n2) para la secuencia 2*x^2 - 1\nIngrese su opcion: "))
+
+    if opcion == 1:
+        secuencia()
+    elif opcion == 2:
+        secuencia2()
