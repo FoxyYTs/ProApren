003b7219a51a243080919841a431bdd4c52f8a85
FoxyYTs <foxy200442@gmail.com>
2026-05-20 21:38:58 -0500

    Modelo 3D Marquesina

--- Universidad/AnalisisNumerico/Secado/camas_secado_3d.py
+++ Universidad/AnalisisNumerico/Secado/camas_secado_3d.py
@@ -26,7 +26,7 @@ H_cumbr = 2.8    # altura cumbrera [m]
 
 # Camas de secado (2 camas paralelas con pasillo central)
 H_cama   = 0.80   # altura de las patas [m]
-W_cama   = 1.60   # ancho de cada cama [m]
+W_cama   = 1.50   # ancho de cada cama [m]
 e_pared  = 0.10   # margen lateral desde la pared [m]
 pasillo  = W_marq - 2*e_pared - 2*W_cama   # ancho del pasillo central [m]
 y_c1     = e_pared                           # borde Y cama 1
